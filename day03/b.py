f995cc71ba53c9da6474ec28f88c3cda6c9aacd0
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-03 13:18:46 +0100

    Improved day03/a.py

--- day03/b.py
+++ day03/b.py
@@ -1,23 +1,19 @@
-import sys, functools, operator
+import sys, math
 
 forest = [
     line.strip()
     for line in sys.stdin
 ]
 
-W, H = len(forest[0]), len(forest)
-
-def count_trees(dx, dy):
-    return sum(
-        forest[dy*i][(dx * i) % W] == '#'
-        for i in range(1, H // dy, 1)
-    )    
+def count_trees(board, dx, dy):
+    w, h = len(board[0]), len(board)
+    return [
+        board[dy * i][(dx * i) % w]
+        for i in range(1, h // dy, 1)
+    ].count('#')
 
 SLOPES = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
-total = functools.reduce(
-    operator.mul, 
-    (count_trees(*s) for s in SLOPES)
-)
+total = math.prod(count_trees(forest, *s) for s in SLOPES)
 
 print(total)
