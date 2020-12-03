1564d3c5ac6da1384c045706088385c7e50f8779
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-03 13:27:49 +0100

    More imrovements

--- day03/b.py
+++ day03/b.py
@@ -8,8 +8,8 @@ forest = [
 def count_trees(board, dx, dy):
     w, h = len(board[0]), len(board)
     return [
-        board[dy * i][(dx * i) % w]
-        for i in range(1, h // dy, 1)
+        line[(dx * i) % w]
+        for i, line in enumerate(board[::dy])
     ].count('#')
 
 SLOPES = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
