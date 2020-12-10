706f8dfa8274b82fb65087a3cf14984ecc22a27b
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-10 12:59:39 +0100

    Add Day 10: Adapter Array

---
+++ day10/b.py
@@ -0,0 +1,17 @@
+import sys
+import functools
+numbers = set(map(int, sys.stdin.read().split()))
+numbers.add(0)
+
+T = max(numbers) + 3
+
+@functools.cache
+def count(n):
+    if n == T:
+        return 1
+    if n not in numbers:
+        return 0
+    return sum(count(n+d) for d in [1,2,3])
+
+    
+print(count(0))
