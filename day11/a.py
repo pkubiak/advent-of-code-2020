434baedd5defb760bc8ae6f50bbd02958fd7c74c
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-12 13:26:05 +0100

    Improvements

--- day11/a.py
+++ day11/a.py
@@ -1,37 +1,32 @@
-import sys
-from copy import deepcopy
+import sys, itertools
 
 
+def simulate(seats):
+    new = {}
+    for (x, y), state in seats.items():
+        occupied = sum(
+            seats.get((x+dx, y+dy)) == '#' 
+            for dx, dy in itertools.product((-1,0,1), repeat=2)
+            if dx != 0 or dy != 0
+        )
+        
+        if state == '#' and occupied >= 4:
+            new[(x, y)] = 'L'
+        elif state == 'L' and occupied == 0:
+            new[(x, y)] = '#'
+        else:
+            new[(x, y)] = state
+
+    return new
 
 
-def simulate(A):
-    B = deepcopy(A)
-    W , H = len(A[0]), len(A)
-    for y in range(H):
-        for x in range(W):
-            if A[y][x] != '.':
-                total = 0
-                for dx in (-1, 0, 1):
-                    for dy in (-1, 0, 1):
-                        if (dx!=0 or dy!=0) and 0 <= x+dx <W and 0<=y+dy <H:
-                            total += (A[dy+y][dx+x] == '#')
-                if A[y][x] == 'L' and total == 0:
-                    B[y][x] = '#'
-                elif A[y][x] == '#' and total >= 4:
-                    B[y][x] = 'L'
-                else:
-                    B[y][x] = A[y][x]
-    return B
+seats = {
+    (x, y): char
+    for y, line in enumerate(sys.stdin)
+    for x, char in enumerate(line.strip())
+}
 
+while seats != (seats := simulate(seats)):
+    pass
 
-seats = [list(line) for line in sys.stdin.read().split('\n')]
-
-while True:
-    n = simulate(seats)
-    if n == seats:
-        break
-    seats = n
-    print('\n'.join([''.join(line) for line in seats]))
-    print()
-
-print(''.join([''.join(line) for line in seats]).count('#'))
+print(list(seats.values()).count('#'))
--- day11/b.py
+++ day11/b.py
@@ -1,50 +1,39 @@
-import sys
-from copy import deepcopy
+import sys, itertools
 
 
-
-
-def check(A, x, y):
-    W , H = len(A[0]), len(A)
+def check(seatch, x, y):
     total = 0
-    for dx in (-1, 0, 1):
-        for dy in (-1, 0, 1):
-            if dx == 0 and dy ==0:
-                continue
+    for dx, dy in itertools.product((-1,0,1), repeat=2):
+        if dx != 0 or dy != 0:
             i = 1
-            while 0 <= x+dx*i < W and 0<=y+dy*i <H:
-                if A[i*dy+y][i*dx+x] == '#':
-                    total += 1
-                    break
-                if A[i*dy+y][i*dx+x] == 'L':
-                    break
+            while seats.get((x + i*dx, y + i*dy)) == '.':
                 i += 1
+            total += seats.get((x + i*dx, y + i*dy)) == '#'
     return total
-                
-def simulate(A):
-    B = deepcopy(A)
-    W , H = len(A[0]), len(A)
-    for y in range(H):
-        for x in range(W):
-            if A[y][x] != '.':
-                total = check(A, x, y)
-                if A[y][x] == 'L' and total == 0:
-                    B[y][x] = '#'
-                elif A[y][x] == '#' and total >= 5:
-                    B[y][x] = 'L'
-                else:
-                    B[y][x] = A[y][x]
-    return B
 
 
-seats = [list(line) for line in sys.stdin.read().split('\n')]
+def simulate(seats):
+    new = {}
+    for (x, y), state in seats.items():
+        occupied = check(seats, x, y)
+        
+        if state == '#' and occupied >= 5:
+            new[(x, y)] = 'L'
+        elif state == 'L' and occupied == 0:
+            new[(x, y)] = '#'
+        else:
+            new[(x, y)] = state
 
-while True:
-    n = simulate(seats)
-    if n == seats:
-        break
-    seats = n
-    # print('\n'.join([''.join(line) for line in seats]))
-    # print()
+    return new
 
-print(''.join([''.join(line) for line in seats]).count('#'))
+
+seats = {
+    (x, y): char
+    for y, line in enumerate(sys.stdin)
+    for x, char in enumerate(line.strip())
+}
+
+while seats != (seats := simulate(seats)):
+    pass
+
+print(list(seats.values()).count('#'))
