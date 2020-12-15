6889d0ceaf06ef0e5a2cf688e94fb8e7bf66dc00
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-15 06:31:25 +0100

    chore: Cleanup

--- day14/b.py
+++ day14/b.py
@@ -3,8 +3,6 @@ import sys, re, itertools
 mask = None
 mem = {}
 
-
-
 for line in sys.stdin.read().split("\n"):
     if m := re.fullmatch("mask = ([X01]{36})", line):
         mask = m.group(1)
@@ -22,11 +20,5 @@ for line in sys.stdin.read().split("\n"):
             ])
             addr2 = int(addr2, 2)
             mem[addr2] = value
-
-        # res = ''.join([
-        #     v if m == 'X' else m
-        #     for m, v in zip(mask, value)
-        # ])
-        # mem[int(addr)] = int(res, 2)
 
 print(sum(mem.values()))
