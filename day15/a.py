e921a4cd08947d38f12f81979dfc5dc3ac29270b
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-15 06:30:59 +0100

    Add Day 15: Rambunctious Recitation

---
+++ day15/a.py
@@ -0,0 +1,20 @@
+import sys
+
+numbers = [int(x) for x in sys.stdin.read().split(',')]
+
+state = {
+    x: i
+    for i, x in enumerate(numbers[:-1], start=1)
+}
+
+last = numbers[-1]
+
+for turn in range(len(numbers), 2020):
+    age = turn - state.get(last, turn)
+
+    state[last] = turn
+
+    last = age
+
+print(age)
+
