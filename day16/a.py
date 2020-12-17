c75cca12982601367299dfce2c6543a4cb8b28fe
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-12-17 06:22:04 +0100

    Simplify code

--- day16/a.py
+++ day16/a.py
@@ -2,22 +2,16 @@ import sys, re
 
 fields, your_ticket, nearby_tickets = sys.stdin.read().split("\n\n")
 
-
 ranges = set()
-for line in fields.split("\n"):
-    m = re.fullmatch(r'(.+): (\d+)-(\d+) or (\d+)-(\d+)', line)
-    _name, a0, a1, b0, b1 = m.groups()
-    ranges.update(range(int(a0), int(a1)+1))
-    ranges.update(range(int(b0), int(b1)+1))
+ranges.update(*[
+    range(int(start), int(end) + 1)
+    for start, end in re.findall(r'(\d+)-(\d+)', fields)
+])
 
-
-total = 0
-for ticket in nearby_tickets.split("\n")[1:]:
-    numbers = map(int, ticket.split(","))
-    total += sum(
-        x
-        for x in numbers
-        if x not in ranges
-    )
+total = sum(
+    int(number)
+    for number in re.findall(r'\d+', nearby_tickets)
+    if int(number) not in ranges
+)
 
 print(total)
