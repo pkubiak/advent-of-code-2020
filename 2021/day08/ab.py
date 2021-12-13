3972a1632abd9b33ba1d7d77da83b9ab4d370a64
Paweł Kubiak <pawel.kubiak@protonmail.com>
2021-12-13 02:57:51 +0100

    fix: Cleaned day08

--- 2021/day08/ab.py
+++ 2021/day08/ab.py
@@ -10,44 +10,45 @@ MAPPING = {
     "abdefg": 6,
     "acf": 7,
     "abcdefg": 8,
-    "abcdfg": 9
+    "abcdfg": 9,
 }
 
-def digit(x, p):
-    x = set(x)
-    y = set()
-    for z, v in zip("abcdefg", p):
-        if v in x:
-            y.add(z)
-    return MAPPING.get(''.join(sorted(y)))
 
-def detect(a, b):
+def decode_digit(x, p):
+    m = dict(zip(p, "abcdefg"))
+    y = "".join(sorted([m[i] for i in x]))
+    return MAPPING[y]
+
+
+def get_mapping(a, b):
     good = []
-    for p in permutations('abcdefg', 7):
-        m = {}
-        for x in a:
-            d = digit(x, p)
-            if d is None:
-                break
-            m[''.join(sorted(x))] = d
-        if len(m) == 10:                
+    for p in permutations("abcdefg", 7):
+        try:
+            m = {"".join(sorted(x)): decode_digit(x, p) for x in a}
             good.append(m)
-    assert len(good) == 1
-    
-    m = good[0]
-    return [m[''.join(sorted(x))] for x in b]
+        except KeyError:
+            continue
 
-with open('input.txt') as file:
-    N = []
+    assert len(good) == 1
+    return good[0]
+
+
+def decode(a, b):
+    m = get_mapping(a, b)
+    return "".join([str(m["".join(sorted(x))]) for x in b])
+
+
+with open("input.txt") as file:
+    N = ""
     suma = 0
     for line in file:
         a, b = line.strip().split(" | ")
         a = a.split(" ")
         b = b.split(" ")
 
-        n = detect(a, b)
-        suma += int(''.join(map(str, n)))
-        N.extend(n)
+        n = decode(a, b)
+        suma += int(n)
+        N += n
 
-total = N.count(1) + N.count(4) + N.count(7) + N.count(8)
-print(total, suma)
+count = N.count("1") + N.count("4") + N.count("7") + N.count("8")
+print(count, suma)
