7a4a15918dd6de111d60a2054be7d3f5314c6558
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-02 11:22:18 +0100

    feat: Updated day02

--- 2022/day02/ab.py
+++ 2022/day02/ab.py
@@ -1,36 +1,38 @@
-POINTS = {
-    "Rock": 1, "Paper": 2, "Scissors": 3,
-    "Win": 6, "Draw": 3, "Lose": 0
+from enum import Enum
+
+class Choice(Enum):
+    Rock = 1
+    Paper = 2
+    Scissors = 3
+
+    def cmp(self, other) -> int: # -1, 0, 1
+        if other == self:
+            return 0
+        return -1 if self.value%3 + 1 == other.value else 1
+
+    def score(self, other) -> int:
+        return self.value + 3 * (self.cmp(other)+1)
+
+
+MAPPING = {
+    "A": "Rock", "B": "Paper", "C": "Scissors",
+    "X": "Rock", "Y": "Paper", "Z": "Scissors"
 }
 
-WINS = {("Rock", "Paper"), ("Paper", "Scissors"), ("Scissors", "Rock")}
-
-def part_a(a, b) -> int:
-    a = {"A": "Rock", "B": "Paper", "C": "Scissors"}[a]
-    b = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}[b]
-
-    return POINTS[b] + POINTS["Draw" if a == b else (a, b) in WINS and "Win" or "Lose"]
+def translate_a(opponent: str, me: str):
+    return Choice[MAPPING[me]], Choice[MAPPING[opponent]]
 
 
-def part_b(a, b) -> int:
-    a = {"A": "Rock", "B": "Paper", "C": "Scissors"}[a]
-    b = {"X": "Lose", "Y": "Draw", "Z": "Win"}[b]
-
-    win = [y for x, y in WINS if a == x][0]
-    lose = [x for x, y in WINS if a == y][0]
-    b2 = a if b == "Draw" else win if b == "Win" else lose
-    return POINTS[b2] + POINTS[b]
+def translate_b(opponent: str, me: str):
+    a = Choice[MAPPING[opponent]]
+    b = {"X": -1, "Y": 0, "Z": 1}[me]
+    b = [i for i in Choice if i.cmp(a) == b][0]
+    return b, a
 
 
 if __name__ == "__main__":
-    total_a = total_b = 0
-    
     with open("input.txt") as file:
-        for line in file:
-            a, b = line.strip().split(" ")
+        items = [line.strip().split(" ") for line in file]
 
-            total_a += part_a(a, b)
-            total_b += part_b(a, b)
-
-    print("a:", total_a)
-    print("b:", total_b)
+    print("a:", sum(Choice.score(*translate_a(*item)) for item in items))
+    print("b:", sum(Choice.score(*translate_b(*item)) for item in items))
