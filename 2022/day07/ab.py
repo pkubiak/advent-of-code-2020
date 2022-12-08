cf1433e0d6e79b001bd30869e93d1f4bd8572f71
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-08 03:00:37 +0100

    refactor: Cleanup using pattern matching

--- 2022/day07/ab.py
+++ 2022/day07/ab.py
@@ -2,42 +2,32 @@ from collections import defaultdict
 from functools import cache
 
 def read_tree(text: str):
-    lines = text.split("\n")
-    i, path, children = 0, [], defaultdict(list)
-    while i < len(lines):
-        _, cmd, *args = lines[i].split(" ")
-        i+= 1
-        if cmd == 'cd':
-            if args[0] == '..':
+    path, tree = [], defaultdict(list)
+    for line in text.splitlines():
+        match line.split():
+            case ["$", "ls"]:
+                pass
+            case ["$", "cd", ".."]:
                 path.pop()
-            else:
-                path.append(args[0])
-        else:
-            while i < len(lines) and not lines[i].startswith('$'):
-                x, y = lines[i].split(" ")
-                if x == 'dir':
-                    x = None
-                else:
-                    x = int(x)
-                children["/".join(path)].append((x, y))
-                i += 1
-
-    return children
-
+            case ["$", "cd", name]:
+                path.append(name)
+            case [size, name]:
+                tree["/".join(path)].append((size, name))
+    return tree
 
 with open("input.txt") as file:
     text = file.read()
-    children = read_tree(text)
+    tree = read_tree(text)
 
     @cache
     def get_size(path):
         total = 0
-        for x, y in children[path]:
-            total += get_size(path + "/" + y) if x is None else x
+        for x, y in tree[path]:
+            total += get_size(path + "/" + y) if x == "dir" else int(x)
 
         return total
 
-    sizes = [get_size(path) for path in children]
+    sizes = [get_size(path) for path in tree]
     print("a:", sum(size for size in sizes if size <= 100_000))
 
     sizes = [size for size in sizes if 70_000_000 - get_size("/") + size > 30_000_000]
