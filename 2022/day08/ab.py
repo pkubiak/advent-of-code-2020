25c246e8b57212c35e4e9775de35fed71f80e23d
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-08 14:47:37 +0100

    refactor: using numpy

--- 2022/day08/ab.py
+++ 2022/day08/ab.py
@@ -1,50 +1,43 @@
-with open("input.txt") as file:
-    text = file.read().split("\n")
-    forest = [[int(i) for i in line] for line in text]
+import numpy as np
 
-    H, W = len(forest), len(forest[0])
-    visible = {}
+def get_views(x, y):
+    return [
+        forest[y,x-1::-1], # left
+        forest[y,x+1:], # right
+        forest[y-1::-1,x], # up
+        forest[y+1:,x] # down
+    ]
 
-def check_visibility():
-    visible = {}
-    xs, ys = list(range(W)), list(range(H))
 
-    for y in ys:
-        for xx in [xs, reversed(xs)]:
-            best = -1
-            for x in xx:
-                if forest[y][x] > best:
-                    visible[x,y] = True
-                best = max(best, forest[y][x])
+def score(v, L):
+    if np.all(L < v):
+        return L.size
+    return np.argmax(L >= v) + 1
 
-    for x in xs:
-        for yy in [ys, reversed(ys)]:
-            best = -1
-            for y in yy:
-                if forest[y][x] > best:
-                    visible[x,y] = True
-                best = max(best, forest[y][x])
-    return len(visible.keys())
 
-def count(x, y):
-    res = 1
-    for dx, dy in [(0,1), (0,-1), (1,0), (-1,0)]:
-        ile = 0
-        for i in range(1,10000):
-            xx = x + i * dx
-            yy = y + i * dy
-            if not (0 <= xx < W and 0 <= yy < H):
-                break
-            ile += 1
-            if forest[yy][xx] >= forest[y][x]:
-                break
-        res *= ile
+def scenic_score(x, y):
+    center = forest[y,x]
+    return np.prod([
+        score(center, view) 
+        for view in get_views(x,y)
+    ])
 
-    return res
 
-print("a:", check_visibility())
-results = []
-for y in range(H):
-    for x in range(W):
-        results.append(count(x, y))
-print("b:", max(results))
+def is_visible(x, y):
+    center = forest[y, x]
+    for view in get_views(x, y):
+        if np.all(center > view):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    text = open("input.txt").read().split("\n")
+    forest = np.array([[int(i) for i in line] for line in text])
+    H, W = forest.shape
+
+    count = sum(is_visible(x, y) for x in range(W) for y in range(H))
+    print("a:", count)
+
+    best = max(scenic_score(x, y) for x in range(1,W-1) for y in range(1,H-1))
+    print("b:", best)
