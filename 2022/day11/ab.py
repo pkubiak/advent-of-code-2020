1ae47924ebd5d5d7d6ad3331f22b1f188e20c884
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-11 23:35:03 +0100

    refactor: Day 11 cleanup

--- 2022/day11/ab.py
+++ 2022/day11/ab.py
@@ -1,37 +1,32 @@
-monkeys = open("input.txt").read().split("\n\n")
 
-print(monkeys)
+import re, math
 
-# parse
-data = []
-for monkey in monkeys:
-    lines = monkey.split("\n")
-    items = list(map(int, lines[1].split(": ")[1].split(", ")))
-    op = lines[2].split(": ")[1]
-    divs = int(lines[3].split("by ")[1])
-    case_true = int(lines[4].split("monkey ")[1])
-    case_false = int(lines[5].split("monkey ")[1])
-    data.append([items, op, divs, case_true, case_false])
+def parse(text):
+    for monkey in text.split("\n\n"):
+        op = re.findall(r"= (.*)", monkey)[0]
+        nums = list(map(int, re.findall(r"\d+", monkey.replace(op,''))))
+        yield [nums[1:-3], op, *nums[-3:]]
+    
 
-from collections import Counter
-M = [x[2] for x in data]
-res = 1
-for m in M:
-    res*=m
-print(M, res)
-counts = Counter()
-from tqdm import trange
-for round in trange(10000):
-    for i in range(len(data)):
-        items = data[i][0]
-        data[i][0] = []
-        for item in items:
-            counts[i] += 1
-            old = item
-            exec(data[i][1])
-            new %= res         
-            x = data[i][3] if (new % data[i][2]) == 0 else data[i][4]
-            data[x][0].append(new)
+def count(monkeys, rounds, fn) -> int:
+    counts = [0] * len(monkeys)
+    for _ in range(rounds):
+        for i, (items, op, div, true, false) in enumerate(monkeys):
+            counts[i] += len(items)
+            for old in items:
+                new = fn(eval(op))
+                x = true if (new % div) == 0 else false
+                monkeys[x][0].append(new)
+            items.clear()
 
-a, b = counts.most_common(2)
-print(a[1]*b[1])
+    counts.sort(reverse=True)
+    return counts[0] * counts[1]
+
+
+text = open("input.txt").read()
+
+print("a:", count(list(parse(text)), 20, lambda x: x//3))
+
+data = list(parse(text))
+M = math.prod(x[2] for x in data)
+print("b:", count(data, 10_000, lambda x: x%M))
