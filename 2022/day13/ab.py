a82e0f8506f2186d5c6d68d203a16feb7c2a44c5
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-13 06:45:56 +0100

    refactor: Day 13 simplified

--- 2022/day13/ab.py
+++ 2022/day13/ab.py
@@ -1,44 +1,28 @@
-with open("input.txt") as file:
-    text = file.read()
+import functools
 
 def cmp(a, b):
     if isinstance(a, int):
         if isinstance(b, int):
-            if a == b:
-                return None
-            return a < b
+            return a-b
         return cmp([a], b)
+
     if isinstance(b, int):
         return cmp(a, [b])
     
     for x, y in zip(a, b):
-        v = cmp(x, y)
-        if v is not None:
+        if v := cmp(x, y):
             return v
-    if len(a) == len(b):
-        return None
-    return len(a) < len(b)
+    return len(a) - len(b)
     
 
-# total = 0
-# for i, pairs in enumerate(text.split("\n\n"), start=1):
-#     l1, l2 = pairs.split("\n")
-#     l1 = eval(l1)
-#     l2 = eval(l2)
+text = open("input.txt").read().replace("\n\n", "\n")
+items = [eval(line) for line in text.split("\n")]
 
-#     if cmp(l1, l2):
-#         total += i
-# print(total)
-items = [eval(line) for line in text.replace("\n\n", "\n").split("\n")]
-items.extend([ [[2]], [[6]]])
-# print(items)
+total = sum(i+1 for i in range(len(items)//2) if cmp(*items[2*i:2*i+2]) < 0)
+print("a:", total)
 
-for _ in range(len(items)):
-    for j in range(1, len(items)):
-        if cmp(items[j-1], items[j]) is True:
-            items[j-1], items[j] = items[j], items[j-1]
+items += [[[2]], [[6]]]
+items.sort(key=functools.cmp_to_key(cmp))
 
-items = items[::-1]
-
-v = (items.index([[2]])+1) * (items.index([[6]])+1)
-print(v)
+v = (items.index([[2]]) + 1) * (items.index([[6]]) + 1)
+print("b:", v)
