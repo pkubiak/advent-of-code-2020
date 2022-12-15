c6af1fe4712d51fbf6754269f8715729dc9bf490
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-15 07:03:29 +0100

    refactor: Day 15 cleanup

--- 2022/day15/ab.py
+++ 2022/day15/ab.py
@@ -8,30 +8,40 @@ for line in text.split("\n"):
     x, y, bx, by = map(int, re.findall(r"[-]?\d+", line))
 
     d = abs(x-bx) + abs(y-by)
-    points.append((x,y,d))
-    beacons.append((bx,by))
+    points += [(x,y,d)]
+    beacons += [(bx,by)]
 
-vals = set()
-LIMIT = 4_000_000
-for Y in range(LIMIT+1):
+def get_ranges(Y):
     ranges = []
-    for x,y,d in points:
-        v = abs(Y-y)
-        if v > d: continue
-        z = d-v
+    for x, y, d in points:
+        z = d - abs(Y - y)
+        if z < 0: continue
         ranges.append((-z+x, -1))
         ranges.append((z+1+x, 1))
-        # vals.update(i+x for i in range(-z, z+1))
-    ranges.sort()
-    ile = 0
-    for x, t in ranges:
-        ile += t
-        if ile == 0 and x <= LIMIT:
-            print(x, Y)
-            print(x *LIMIT+ Y)
-    # for x,y in beacons:
-    #     if y == Y:
-    #         vals.discard(x)
-    
-print(len(vals))
+    return ranges
 
+
+def p1(Y):
+    vals = set()
+    r = get_ranges(Y)
+    for i in range(0,len(r),2):
+        vals.update(range(r[i][0], r[i+1][0]))
+
+    for x, y in beacons:
+        if y == Y:
+            vals.discard(x)
+
+    print("a:", len(vals))
+
+
+def p2(limit):
+    for y in range(limit + 1):
+        ranges = get_ranges(y)
+        ile = 0
+        for x, t in sorted(ranges):
+            ile += t
+            if ile == 0 and 0 <= x <= limit:
+                print("b:", x * limit + y)
+
+
+p1(2_000_000) or p2(4_000_000)
