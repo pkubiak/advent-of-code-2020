3c87620db02f9e7cf2f524220ba56b87e5769924
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-21 09:10:38 +0100

    refactor: Use lambda hack

--- 2022/day21/ab.py
+++ 2022/day21/ab.py
@@ -1,37 +1,20 @@
-def calculate(values, key):
-    match values[key]:
-        case [a, op, b]:
-            a = calculate(values, a)
-            b = calculate(values, b)
-            return eval(f"a {op} b")
-        case x:
-            return x
+import re
+text = open("input.txt").read()
 
+text = re.sub(r"\b([a-z]{4})\b", r"\1()", text)
+text = re.sub("([a-z]{4})\(\):", r"\1=lambda:", text)
+exec(text)
 
-def check(value):
-    values['humn'] = value
-    return calculate(values, 'root')
+print("a:", int(root()))
 
+exec(re.sub('[-+*/]', '-', re.findall("root=.*", text)[0]))
 
-values = {}
-for line in open("input.txt"):
-    a, b = line.strip().split(": ")
-    if b.isnumeric():
-        values[a] = int(b)
-    else:
-        values[a] = b.split(" ")
-
-# part1
-print("a:", int(calculate(values, 'root')))
-
-# part2
-values['root'][1] = '-'
 a, b = 0, 10000000000000
-assert (check(a) > 0) and (check(b) < 0)
 
 while a+1 < b:
     s = (a+b)//2
-    if check(s) >= 0:
+    exec(f"humn = lambda: {s}")
+    if root() >= 0:
         a = s
     else:
         b = s
