5aebb82fe4a079ea48fdb562ba2ceee59d1aa0c9
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-22 08:05:24 +0100

    refactor: Day 22 cleanup

--- 2022/day22/ab.py
+++ 2022/day22/ab.py
@@ -1,84 +1,81 @@
 import re
 
+TELEPORTS = {}
 DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-SIDES = {
-    (1,0): 'A', (2,0): 'B',
-    (1,1): 'C',
-    (0,2): 'D', (1,2): 'E',
-    (0,3): 'F'
-}
+SIDES = {"A": (1, 0), "B": (2, 0), "C": (1, 1), "D": (0, 2), "E": (1, 2), "F": (0, 3)}
 
+# read input
 text = open("input.txt").read()
 
 mapa, moves = text.split("\n\n")
-mapa = mapa.split("\n")
-L = len(mapa) // 4
-mapa = {(x, y): c for y, line in enumerate(mapa) for x, c in enumerate(line) if c in '#.'}
+mapa = {
+    (x, y): c for y, line in enumerate(mapa.split("\n")) for x, c in enumerate(line) if c in "#."
+}
+L = round((len(mapa)//6)**0.5)
 
 moves = re.findall("(\d+|[LR])", moves)
 
 
-def generate(side, dir):
-    s = [k for k,v in SIDES.items() if v==side][0]
-    x0, y0 = s[0]*L, s[1]*L
-    if dir == 'top':
-        return [(x0+i, y0) for i in range(L)]
-    if dir == 'right':
-        return [(x0+L-1, y0+i) for i in range(L)]
-    if dir == 'bottom':
-        return [(x0 +L-1-i, y0+L-1) for i in range(L)]
-    if dir == 'left':
-        return [(x0, y0+L-1-i) for i in range(L)]
+def generate(side, edge):
+    """Generate cells on side's edge."""
+    s = SIDES[side]
+    x0, y0 = s[0] * L, s[1] * L
+    dir = DIRS[["top", "right", "bottom", "left"].index(edge)]
+    if edge in ("right", "bottom"):
+        x0 += L - 1
+    if edge in ("bottom", "left"):
+        y0 += L - 1
+    return [(x0 + i * dir[0], y0 + i * dir[1]) for i in range(L)]
 
-TELEPORTS = {}
 
-def match(d0, pos1, d1, pos2):
-    for a, b in zip(pos1, pos2):
+def connect(side0, edge0, side1, edge1):
+    EDGES = ["right", "bottom", "left", "top"]
+    d0 = EDGES.index(edge0)
+    d1 = (EDGES.index(edge1) + 2) % 4
+    for a, b in zip(generate(side0, edge0), reversed(generate(side1, edge1))):
         assert (a[0], a[1], d0) not in TELEPORTS
         TELEPORTS[(a[0], a[1], d0)] = (b[0], b[1], d1)
-        TELEPORTS[(b[0], b[1], (d1+2)%4)] = (a[0], a[1], (d0+2)%4)
-
-
-match(3, generate('A', 'top'), 0, reversed(generate('F','left')))
-match(2, generate('A', 'left'), 0, reversed(generate('D', 'left')))
-match(2, generate('C', 'left'), 1, reversed(generate('D', 'top')))
-match(0, generate('C', 'right'), 3, reversed(generate('B','bottom')))
-match(0, generate('B','right'), 2, reversed(generate('E', 'right')))
-match(3, generate('B', 'top'), 3, reversed(generate('F','bottom')))
-match(1, generate('E', 'bottom'), 2, reversed(generate('F', 'right')))
+        TELEPORTS[(b[0], b[1], (d1 + 2) % 4)] = (a[0], a[1], (d0 + 2) % 4)
 
 
 def crawl(mapa, moves, overflow_method):
-    x = y = d = 0 
+    x = y = d = 0
     while (x, y) not in mapa:
-        x+=1
+        x += 1
     for move in moves:
-        if move in 'LR':
-            d = (d + (1 if move == 'R' else -1)) % 4
+        if move in "LR":
+            d = (d + (1 if move == "R" else -1)) % 4
             continue
         length = int(move)
 
         for i in range(length):
-            x2, y2, d2 = (x+DIRS[d][0]), (y + DIRS[d][1]), d
+            x2, y2, d2 = (x + DIRS[d][0]), (y + DIRS[d][1]), d
 
             if (x2, y2) not in mapa:
-                if overflow_method == 'cube':
+                if overflow_method == "cube":
                     assert (x, y, d) in TELEPORTS, f"{x}, {y}, {d}"
-                    x2, y2, d2 = TELEPORTS[x,y,d]
-                elif overflow_method == 'normal':
+                    x2, y2, d2 = TELEPORTS[x, y, d]
+                elif overflow_method == "normal":
                     while True:
-                        x2 += DIRS[(d+2)%4][0]
-                        y2 += DIRS[(d+2)%4][1]
+                        x2 += DIRS[(d + 2) % 4][0]
+                        y2 += DIRS[(d + 2) % 4][1]
                         if (x2, y2) not in mapa:
                             break
-                    x2 += DIRS[d%4][0]
-                    y2 += DIRS[d%4][1]
+                    x2 += DIRS[d % 4][0]
+                    y2 += DIRS[d % 4][1]
 
-            if mapa[x2,y2] == '#':
+            if mapa[x2, y2] == "#":
                 break
             x, y, d = x2, y2, d2
-        
-    return 1000 * (y+1) + 4 * (x+1) + d
+    return 1000 * (y + 1) + 4 * (x + 1) + d
 
-print("a:", crawl(mapa, moves, 'normal'))
-print("b:", crawl(mapa, moves, 'cube'))
+connect("A", "top", "F", "left")
+connect("A", "left", "D", "left")
+connect("B", "bottom", "C", "right")
+connect("B", "right", "E", "right")
+connect("B", "top", "F", "bottom")
+connect("C", "left", "D", "top")
+connect("E", "bottom", "F", "right")
+
+print("a:", crawl(mapa, moves, "normal"))
+print("b:", crawl(mapa, moves, "cube"))
