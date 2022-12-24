4732521487cc1b7d4bcbf868b1a27bed7db19dea
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-12-24 07:43:26 +0100

    refactor: Day24 speedup

--- 2022/day24/ab.py
+++ 2022/day24/ab.py
@@ -1,4 +1,5 @@
 import math
+from collections import deque
 
 text = open("input.txt").read()
 
@@ -12,38 +13,35 @@ W, H = len(lines[0]), len(lines)
 PERIOD = math.lcm(W-2, H-2)
 
 DIRS = {'<': (-1, 0), '>': (1,0), '^': (0, -1), 'v': (0, 1), 'x': (0,0)}
+BLIZARDS = {}
 
 def simulate(blizards):
     new = set()
     for x, y, dir in blizards:
         dx, dy = DIRS[dir]
-        x2, y2 = x+dx, y+dy
-        if x2 == 0:
-            x2 = W-2
-        if x2 == W-1:
-            x2 = 1
-        if y2 == 0:
-            y2 = H-2
-        if y2 == H-1:
-            y2 = 1
+        x2 = (x+dx-1)%(W-2)+1
+        y2 = (y+dy-1)%(H-2)+1
         new.add((x2,y2,dir))
     return new
 
+
 def BFS(blizards, target):
-    queue = [(1, 0, 0, 0)]
+    queue = deque([(1, 0, 0, 0)])
     bests = {}
     bests[queue[-1]] = 0
     last_time = -1
-
+    ile = 0
     while queue:
-        item, *queue = queue
+        ile += 1
+        item = queue.popleft()
         x, y, time, count = item
-        # print(len(queue), count)
 
         if time != last_time:
             last_time = time
-            blizards = simulate(blizards)
-            cells = {(x,y) for x,y,_ in blizards}
+            if time not in BLIZARDS:
+                b =simulate(blizards)
+                BLIZARDS[time] = (b, {(x,y) for x,y,_ in b})
+            blizards, cells = BLIZARDS[time]
 
         if (x,y,count) == target:
             return bests[item]
@@ -55,12 +53,12 @@ def BFS(blizards, target):
             new_count += 1
 
         for dx, dy in DIRS.values():
-            x2, y2 = x+dx, y+dy
+            p = x2, y2 = x+dx, y+dy
             
-            if (x2,y2)==(1,0) or (x2,y2) == (W-2, H-1) or ((1 <= x2 < W-1) and (1 <= y2 < H-1)):
-                item2 = (x2, y2, (time+1), new_count)
+            if p==(1,0) or p == (W-2, H-1) or ((1 <= x2 < W-1) and (1 <= y2 < H-1)):
+                item2 = (x2, y2, (time+1)%PERIOD, new_count)
 
-                if ((x2, y2) not in cells) and (item2 not in bests):
+                if (p not in cells) and (item2 not in bests):
                     queue.append(item2)
                     bests[item2] = bests[item]+1
 
